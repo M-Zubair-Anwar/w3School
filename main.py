1ba4df03698359568a780e8bd92c4b758355ba3e
zM-Zubair-Anwar <zubairanwar2222@gmail.com>
2023-08-18 11:51:57 +0500

    commit the data type

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-# python intro
-# first code
-print("hello worlds!")
+# python data type
+
+# Text Type:	    str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	    dict
+# Set Types:	    set, frozenset
+# Boolean Type:	    bool
+# Binary Types:	    bytes, bytearray, memoryview
+# None Type:	    NoneType
+
+# getiing data types
+x=5
+print(type(x))
+a="ali"
+print(type(a))
+s=10.9
+print(type(s))
+d=[12,3,6,7]
+print(type(d))
+
+#convert of one type to another
+x=5
+print(float(x))
+print(str(x))
+print(complex(x))
+
+w=6.3
+print(int(w))
