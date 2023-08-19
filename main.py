dae3fe4bc33c4f5fd6e33c8cf224c6c2fac91764
zM-Zubair-Anwar <zubairanwar2222@gmail.com>
2023-08-19 13:26:23 +0500

    commit the operator

--- main.py
+++ main.py
@@ -1,66 +1,65 @@
-# string
-# strings in python are surrounded by either single quotation or double quotation marks
-# "heloo"  is same 'helo'
-import string
+# operator and list
+# operator are used to perfome operations on variable and  values.
+# exp
+print(10+3)                                # 13
 
-print("helo")
-print('heloo')
+#1 Python divides the operators in the following groups:
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
 
-#1 multiple string:
-# we can assign multiple string to paragraph
-a="""There are some key Difference Between Local 
-and Global Variable in Python: Global variables are declared 
-outside the functions whereas 
-local variables are declared within the functions. """
-print(a)
+# lists
+# lists are use to store multiple item in a single variable.
+# lists are created to use square bracket.list items are ordered,changeable and allow
+# to duplicate.
+list=["apple","banana","cherry','apple"]
+print(list)                                   # ['apple', 'banana', "cherry','apple"]
+print(len(list))                              # 3
+print(type(list))                             # <class 'list'>
+# for index
+print(list[1])                                # banana
+print(list[-1])                               # cherry','apple
+# for range
+print(list[2:3])                               # ["cherry','apple"]
+print(list[-1:-3])                             # []
+print(list[:3])                                # ['apple', 'banana', "cherry','apple"
+print(list[2:])                                # ["cherry','apple"]
+# check if item is exists
+list=["apple","banana","cherry','apple"]
+if "apple" in  list:
+    print("yes")                                # yes
+# change the item
+list=["apple","banana","cherry','apple"]
+list[1]="mango"
+print(list)                                     # ['apple', 'mango', "cherry','apple"]
 
-#2 string slicing
-e="heloo world"
-print(e[2:5])     # position 2 to position 5(5 not included)
-print(e[:5])      # position 0 to position 5 (5 not included)
-print(e[2:])      # position 2 to position end
-print(e[-5:-2])   # this is reverse slicing starting from reverse.last integer included
+list=["apple","banana","cherry"]
+list[1:2]=["mango","watermelon"]
+print(list)                              #['apple', 'mango', 'watermelon', 'cherry']
 
-#3 modify string
-# upper case
-q=' Hello World '
-print(q.upper())                        #for  upper case
-print(q.lower())                        #for  lower case
-print(q.strip())                        #remove white space from beginning or end
-print(q.replace("H","j"))    # for using the replace the world
-print(q.split(','))                     #for split the string
+list.insert(2,"grapes")
+print(list)                      #['apple', 'mango', 'grapes', 'watermelon', 'cherry']
 
-#5 string concatination
-a="h"
-b="eloo"
-print(a+b)                             # result is heloo
+list.append("orange")
+print(list)       #['apple', 'mango', 'grapes', 'watermelon', 'cherry', 'orange']
 
-#6 formating string
-age=30
-name="my name is ali and i am  {}"
-print(name.format(age))               # my name is ali and i am  30
+# extend list
+fruits=["apple","banan","orange"]
+vagetale=["okra","spinach"]
+fruits.extend(vagetale)
+print(fruits)              #['apple', 'banan', 'orange', 'okra', 'spinach']
 
-name="ahmad"
-age=23
-person="i am {} and my age is {}"
-print(person.format(name,age))  # i am ahmad and my age is 23
+# remove
+fruits.remove("apple")
+print(fruits)               #['banan', 'orange', 'okra', 'spinach']
 
-name="ali"
-age=20
-person="i am {1} and my age is {0}"
-print(person.format(name,age))  # i am 20 and my age is ali
+# remove specific in index
+# if you do not mention the index than pop remove the last item.
+fruits.pop(1)
+print(fruits)              #['banan', 'okra', 'spinach']
 
-#7 escape characer
-# escape character allow to insert character that are illegal in string.
-# a="my name "first" is ali"
-# print(a)
-a="my name \"first\" is ali"
-print(a)
-# Escape Sequence	   Meaning
-# \\	               Backslash
-# \n	               Newline
-# \r	               Carriage Return
-# \t	               Horizontal Tab
 
-#8 string method
-# there are many built in method that can use in sting
