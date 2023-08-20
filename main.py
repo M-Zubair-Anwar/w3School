81ce2cf0626fa2f111383d44fd3603f22ef85fca
zM-Zubair-Anwar <zubairanwar2222@gmail.com>
2023-08-20 15:26:17 +0500

    commit the set

--- main.py
+++ main.py
@@ -1,42 +1,106 @@
-# booleans
-#1 booleans present on of two value ture or false
-print(10>9)                      # true
-print(10==9)                     # false
-print(10<9)                      # false
+# set
+#set are used to store multiple items in on variable .
+# a set is colletion which is unordered.unchangeable and unindexed
+# every time it print defferent result
+# ture and one is consider the same
+set={"ali","header","rahman"}
+print(set)                             # {'rahman', 'header', 'ali'}
 
-a=122
-b=100
-if a>b:
-    print("a is greter than b")  # if a greater than this will print
-else:
-    print("b is greter than a")  # otherwise this will be printed
+# dulicate not allowed
+name={"ali","header","rahman","ali"}
+print(name)                            # {'ali', 'header', 'rahman'}
+print(len(name))                       # 3
+print(type(name))                      # <class 'set'>
 
-#2 boolean ture value
-print(bool(5))                   # true
-print(bool("ali"))               # true
-print(bool(10.2))                # true
+# na=set(("ali","header","rahman","ali"))
+# print(na)                            ??????????  not run pycham
 
-# boolean false value
-print(bool(False))               # False
-print(bool(None))                # False
-print(bool(0))                   # False
-print(bool(''))                  # False
-print(bool({}))                  # False
-print(bool([]))                  # False
-print(bool(()))                  # False
+# LOOP IN SET
+name={"ali","header","rahman","ali"}
+for x in name:
+    print(x)
 
-# example
-def my_fun():
-    return True
-print(my_fun())                  # true
+# value accurance
+name={"ali","header","rahman","ali"}
+for x in name:
+    if "ali" in x:
+        print("yes")                       # yes
+    # else:
+    #     print("na")
+# 2nd way
+print("ali" in name)                       # True
 
-def fun():
-    return False
-if fun():
-        print("yes")
-else:
-        print("no")              # no
+# ADD METHOD
+name={"ali","header","rahman","ali"}
+name.add("sohan")
+print(name)                               # {'sohan', 'header', 'ali', 'rahman'}
+# 2 SET ADD
+name={"ali","header","rahman","ali"}
+num={"mudassar","raja"}
+num.update(name)
+print(num)                        # {'rahman', 'raja', 'header', 'ali', 'mudassar'}
 
-s=100
-print(isinstance(s,str))         # False
-print("change")
+# REMOVE SET ITEM
+name={"ali","header","rahman","adal"}
+name.remove("rahman")
+print(name)                                  # {'ali', 'header', 'adal'}
+
+name={"ali","header","rahman","adal"}
+name.discard("header")
+print(name)                                  # {'ali', 'rahman', 'adal'}
+
+# pop method reove a random item
+name={"ali","header","rahman","adal"}
+name.pop()
+print(name)                                  # {'ali', 'rahman', 'header'}
+
+# clear method empties the set
+name={"ali","header","rahman","adal"}
+name.clear()
+print(name)                                  # set()
+
+# delete
+# name={"ali","header","rahman","adal"}
+# del name
+# print(name)                                  # this will raise error
+
+# JOIN SET
+name={"ali","header","rahman","ali"}
+num={"mudassar","raja"}
+n3=name.union(num)
+print(n3)                     #{'ali', 'rahman', 'mudassar', 'raja', 'header'}
+
+# duplicates
+x={"ali","header","rahman","ali"}
+y={"mudassar","raja","ali"}
+x.intersection_update(y)
+print(x)                                      # {'ali'}
+
+x={"mudassar","header","rahman","ali"}
+y={"mudassar","raja","ali"}
+z=x.intersection(y)
+print(z)                                     # {'mudassar', 'ali'}
+
+# KEP ALL BUT NOT DUBLICATE
+x={"mudassar","header","rahman","ali"}
+y={"mudassar","raja","ali"}
+x.symmetric_difference_update(y)
+print(x)                                   # {'header', 'raja', 'rahman'}
+
+x={"mudassar","header","rahman","ali"}
+y={"mudassar","raja","ali"}
+z=x.symmetric_difference(y)
+print(z)                                    # {'raja', 'header', 'rahman'}
+
+
+
+
+
+
+
+
+
+
+
+
+
