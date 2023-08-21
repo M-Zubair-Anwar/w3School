b4ca61f52f74132234ae9568149c3c0e6acc6376
zM-Zubair-Anwar <zubairanwar2222@gmail.com>
2023-08-21 15:51:36 +0500

    commit the function

--- main.py
+++ main.py
@@ -1,102 +1,67 @@
-# LOOPING
-# python have two loop commands
-# 1 for loop
-# 2 while loop
+# FUNCTION
+# A functions is block of code which is only run when it is called.
+# you can pass data known as parameters in to funtion .a function
+# can return data as a result.
+def fun ():
+    print("hello")
+fun()                                  # hello
 
-# WHILE LOOP
-i=0
-while i<6:
-    print(i)
-    i=i+1                      # start from 1 to 5
-# or
-i=0
-while i<6:
-    print(i)
-    i +=1                     # start from 0 to 5
+# ARGUMENR
+def name(sname):
+    print(sname,"cricketer")
+name("ali")                             # ali cricketer
+name("adal")                            # adal cricketer
+name("hassn")                           # hassn cricketer
 
-# BREAK
-i=0
-while i<6:
-    print(i)
-    if i==4:
-        break
-    i +=1
+def nam(name,lname):
+    print(name+" "+lname)
+nam("jone","cris")           # jone cris
 
-# CONTINUE
-i=0
-while i<6:
-    i += 1
-    if i==3:
-        continue
-    print(i)
+# * ARGS
+def boy(*name):
+    print("the youngest child is" + name[1])
+boy("ali","ahsan","rehan",)
 
-# WITH ELSE
+def boy(ch1,ch2,ch3):
+    print("the youngest child is"+" " + ch3)
+boy(ch1 = "ali",ch2="ahsan",ch3="rehan",)
 
-i=0
-while i<6:
-    print(i)
-    i +=1
-else:
-    print("i is no longer less than 6")
 
-# PYTHON FOR LOOP
+def boy(**name):
+    print("the youngest child is" + name["lname"])
+boy(fname="ali",lname= " dogar")
 
-lit=["ali", "adal", "rehman"]
-for s in lit:
-    print(s)
+# DEFALT PERAMETER VALUE
+def location(country="pakistan"):
+    print("i am from "+ country)
+location("sweden")
+location("turky")
+location("usa")
+location()
 
-# string looping
-for a in "rahman":
-    print(a)
+def list(food):
+    for s in food:
+        print(s)
+friuts=["apple","banana","orange"]
+list(friuts)
 
-lit=["ali","adnan", "adal", "rehman"]
-for s in lit:
-    print(s)
-    if s=="adal":
-        break                       # print 3  name
+# RETURN
+def count(x):
+    return 5*x
+print(count(3))
 
-# or
-lit=["ali","adnan", "adal", "rehman"]
-for s in lit:
-    if s =="adal":
-        break
-    print(s)                            # print 2 name
 
-# continue
-lit=["ali","adnan", "adal", "rehman"]
-for s in lit:
-    if s =="adal":
-        continue
-    print(s)
 
-# RANGE
-for e in range(8):
-    print(e)
-# or
-for w in range(2,6):
-    print(w)
-# or
-for w in range(2,12,2):
-    print(w)
 
-#else
-for q in range(6):
-    print(q)
-else:
-    print("done")
-# with break
-for q in range(6):
-    if q==4:
-        break
-    print(q)
-else:
-    print("done")
-# NESTED
-lit=["ali","adnan", "adal", "rehman"]
-lit1=[ "adal", "rehman"]
-for q in lit:
-    for w in lit1:
-        print(q,w)
+
+
+
+
+
+
+
+
+
 
 
 
